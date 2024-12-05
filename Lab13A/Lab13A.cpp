d77d2e91577ab790a517ffdfabdb504ffe5a8af2
Chase McCluskey <c-mccluskey.1@onu.edu>
2024-12-05 09:19:33 -0500

    Project Lab13A Finsihed

--- Lab13A/Lab13A.cpp
+++ Lab13A/Lab13A.cpp
@@ -1,8 +1,12 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
+
+void pressEnterToContinue(void);
 
 int main() {
 	ifstream in_File;
@@ -15,24 +19,52 @@ int main() {
 
 	char ch;
 	char compare;
+	int counterLetters = 0;
 
-
-	for (int i = 0; i <= 26; i++) {
-
-		int counterLetters = 0;
-
-		switch (i) {
-		case 1: compare = 'A'; case 2:compare = 'B'; case 3:compare = 'C'; case 4:compare = 'D'; case 5:compare = 'E'; case 6:compare = 'F'; case 7:compare = 'G'; case 8:compare = 'H'; case 9:compare = 'I'; case 10:compare = 'J';
-		case 11:compare = 'K'; case 12:compare = 'L'; case 13:compare = 'M'; case 14:compare = 'N'; case 15:compare = 'O'; case 16:compare = 'P'; case 17:compare = 'Q'; case 18:compare = 'R';  case 19:compare = 'S'; case 20:compare = 'T';
-		case 21:compare = 'U'; case 22:compare = 'V'; case 23:compare = 'W'; case 24:compare = 'X'; case 25:compare = 'Y'; case 26:compare = 'Z';
+	while (!in_File.eof()) {
+		in_File.get(ch);
+		if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+			counterLetters++;
 		}
+	}
+	in_File.close();
+
+	cout << "Total number of letters: " << counterLetters << endl;
+	cout << "Frequency: " << endl;
+
+
+
+	int upperAscii = 65;
+	int lowerAscii = 97;
+
+	for (int i = 0; i < 26; i++) {
+	
+		in_File.open(filename);
+		int characters=0;
+		compare = upperAscii;
+
 		while (!in_File.eof()) {
 
 			in_File.get(ch);
-			if (toupper(ch) == compare) {
-
+			if (ch == upperAscii || ch == lowerAscii) {
+				characters++;
 			}
 
 		}
+		double percent = ((double) characters / counterLetters) * 100;
+		cout << "'" << compare << "': " << fixed << setprecision(3) << percent << endl;
+		upperAscii++;
+		lowerAscii++;
+		in_File.close();
+		//continue;
 	}
+
+	pressEnterToContinue();
+}
+void pressEnterToContinue(void) {
+	char c;
+	cout << "Press Enter to continue...";
+	cin.ignore(1024, '\n');
+	do { cin.get(c); } while (c != '\n' && c != EOF);
+	return;
 }
